82ecfb9fca2d0c165ab5dbc1a3e4d8ed3822c497
Todd Hayes <tahdhaze09@gmail.com>
2021-11-24 16:01:54 -0500

    1st python lesson for real

---
+++ helloPython.py
@@ -0,0 +1,46 @@
+# this is a comment
+name = 'Todd'
+age = 33
+is_fit = False
+speed_factor = 0.56
+j = 34
+current_speed = speed_factor * j
+str_to_int = '35'
+complex_no = 32+5j
+my_cars = ['AMC', 'Audi', 'BMW']
+tupling = ('Ed', 'Charles', 'Mike')
+my_set = {'Renault', 'McLaren', 'Ferrari'}
+footy_dict = {'goals': 22, 'team':'MCY'}
+my_team = input('What is your fave team?')
+
+print(name + ' is my name.')
+print(name + ' is ' + str(age) + ' years old.')
+print(int(str_to_int))
+print(age)
+print(complex_no)
+print(my_cars)
+print(my_set)
+print(footy_dict)
+print(current_speed)
+print(my_team)
+print(type(age))
+print(type(name))
+print(type(is_fit))
+print(type(complex_no))
+print(type(my_cars))
+my_cars.append('Cadillac')
+print(my_cars)
+my_cars[1] = 'Dodge'
+print(my_cars)
+print(type(tupling))
+print(type(my_set))
+print(type(footy_dict))
+print(type(current_speed))
+if(age >= j):
+    print("Age is bigger than j")
+else:
+    print("Your variables are all messed up")
+if('Cadillac' in my_cars):
+    print('you got a Cadillac!!!')
+for car in my_cars:
+    print(car)
